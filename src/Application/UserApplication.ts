c7b573d44806dd6177cc256cb7df06516e710581
Fagner de Oliveira Bernardo <bernardo.fagner@hotmail.com>
2021-10-12 23:53:08 -0300

    modificando estrutura de pastas

--- src/Application/UserApplication.ts
+++ src/Application/UserApplication.ts
@@ -1,5 +1,5 @@
+import TUserApplication from "../ApplicationCore/TUserApplication";
 import TAccreditedUser from "../Entities/ClassBased/User";
-import TUserApplication from "./ApplicationCore/TUserApplication";
 
 /**Aplicação da primeira e segunda lei Solid - Open-Close Principle. Neste exemplo, não importa o tipo de usuário, a credencial correta sempre será exibida.
  * 
--- src/ApplicationCore/TUserApplication.ts
+++ src/ApplicationCore/TUserApplication.ts
@@ -1,4 +1,4 @@
-import TAccreditedUser from "../../Entities/ClassBased/User";
+import TAccreditedUser from "../Entities/ClassBased/User";
 
 export default interface TUserApplication {
     Execute(user: TAccreditedUser): any;
