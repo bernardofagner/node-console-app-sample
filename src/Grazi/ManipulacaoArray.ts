670c4a058a33231896f2de582a969f2a69165cd0
Grazielle Amanda do Carmo Café <grazielle.cafe@dtidigital.com.br>
2021-10-22 00:14:29 -0300

    Teste de resolução do exercício 02. Verificar callback function.

--- src/Grazi/ManipulacaoArray.ts
+++ src/Grazi/ManipulacaoArray.ts
@@ -1,18 +1,56 @@
 export class ManipulacaoArray {
     public A: Array<number> = [1, 3, 5, 4, 7, 6, 2, 8, 5, 9];
-
+    public B: Array<number> = [4, 3, 5, 6, 1, 3, 2, 4, 1, 7, 5, 6, 4];
+  
     public Run(): void {
-        const value = 4;
-        // Math.round(Math.random()*10);
-        console.log(this.encontrarValorNoArray(value));
+        const value = Math.round(Math.random()*20);
+        console.log('Resposta:', this.encontrarUmElementoNoArraySemRepeticao(value), '\n');
+
+        this.criarVetorDeElementosNaoRepetidos();
     }
 
-    private encontrarValorNoArray(valorNumerico: number): boolean {
+    // Não pode usar array.prototype.find()
+    private encontrarUmElementoNoArraySemRepeticao(elemento: number): boolean {
+        console.log(`Array sem repetição: [${this.A}]`);
+        console.log('O valor aleatório a ser buscado no array é:', elemento);
+
         for(let i = 0; i < this.A.length; i++){
-            if(valorNumerico === this.A[i]) {
+            if(this.A[i] === elemento) {
                 return true;
             }
         }
         return false;
     }
+
+    /* Criar um vetor de elementos não repetidos a partir dos elementos de um vetor com elementos repetidos. Não pode usar array.prototype.filter() */
+    private criarVetorDeElementosNaoRepetidos(): void {
+        const vetorOriginal = this.B;
+        console.log(`Vetor original: [${vetorOriginal}]`);
+
+        const vetorCriadoSemRepeticao: Array<number> = [];
+        console.log('Criação vetor: ', vetorCriadoSemRepeticao);
+
+        for(let i = 0; i < vetorOriginal.length; i++){
+            if(!this.encontrarUmElementoNoArraySemRepeticao(vetorOriginal[i]) === true){
+                vetorCriadoSemRepeticao.push(vetorOriginal[i]);
+            }
+        }
+    }
+
+// se  \    
+    // private criarVetorDeElementosNaoRepetidos(): void{
+    //     const vetorFiltrado: Array<number> =  [];
+    //     let i = 0;
+
+    //     for(let i = 0; i < this.encontrarValorNoArray.length; i++){       
+    //     } 
+
+    //     console.log('Vetor filtrado', vetorFiltrado);
+    // }
 }
+
+// if(vetorFiltrado != this.encontrarValorNoArray[i]) 
+// vetorFiltrado.push(this.B[i]); 
+//     if(this.B[i] !== vetorFiltrado[]){
+//         vetorFiltrado.push(this.B[i]);
+//     } 
